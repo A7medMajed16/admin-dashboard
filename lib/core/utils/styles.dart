250b215400bda8f0014ca636f0cec1722fbfd687
A7medMajed16 <ahmedbark129@gmail.com>
2024-03-24 12:35:17 +0200

    Add styles file and Google fonts Package

---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+abstract class Styles {
+  static TextStyle textStyle24W600 = GoogleFonts.montserrat(
+    color: const Color(0xff064061),
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+  );
+  static TextStyle textStyle20W600 = GoogleFonts.montserrat(
+    color: const Color(0xff064061),
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+  );
+  static TextStyle textStyle20W500 = GoogleFonts.montserrat(
+    color: const Color(0xffFFFFFF),
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+  );
+  static TextStyle textStyle18W600 = GoogleFonts.montserrat(
+    color: const Color(0xff4EB7F2),
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+  );
+  static TextStyle textStyle16W700 = GoogleFonts.montserrat(
+    color: const Color(0xff4EB7F2),
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+  );
+  static TextStyle textStyle16W600 = GoogleFonts.montserrat(
+    color: const Color(0xff064061),
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+  );
+  static TextStyle textStyle16W500 = GoogleFonts.montserrat(
+    color: const Color(0xff064061),
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+  );
+  static TextStyle textStyle16W400 = GoogleFonts.montserrat(
+    color: const Color(0xff064061),
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+  );
+  static TextStyle textStyle14W400 = GoogleFonts.montserrat(
+    color: const Color(0xffAAAAAA),
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+  );
+  static TextStyle textStyle12W400 = GoogleFonts.montserrat(
+    color: const Color(0xffAAAAAA),
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+  );
+}
