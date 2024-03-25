01108b01fff596c6cdd1e76e37ac83566cce1578
A7medMajed16 <ahmedbark129@gmail.com>
2024-03-25 14:51:11 +0200

    "Add setting and logout items to custom drawer"

--- lib/features/dashboard/presentation/widgets/sections/drawer_section/custom_drawer.dart
+++ lib/features/dashboard/presentation/widgets/sections/drawer_section/custom_drawer.dart
@@ -1,5 +1,8 @@
+import 'package:admin_dashboard/core/utils/app_icons.dart';
+import 'package:admin_dashboard/features/dashboard/data/models/drawer_item_model.dart';
 import 'package:admin_dashboard/features/dashboard/data/models/user_model.dart';
 import 'package:admin_dashboard/features/dashboard/presentation/widgets/sections/drawer_section/custom_drawer_list.dart';
+import 'package:admin_dashboard/features/dashboard/presentation/widgets/sections/drawer_section/drawer_item.dart';
 import 'package:admin_dashboard/features/dashboard/presentation/widgets/sections/drawer_section/user_info.dart';
 import 'package:flutter/material.dart';
 
@@ -10,14 +13,32 @@ class CustomDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      child: Column(
-        children: [
-          UserInfo(userModel: userModel),
-          const SizedBox(
-            height: 8,
-          ),
-          const CustomDrawerList(),
-        ],
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 28, vertical: 48),
+        child: Column(
+          children: [
+            UserInfo(userModel: userModel),
+            const SizedBox(
+              height: 8,
+            ),
+            const CustomDrawerList(),
+            const Expanded(child: SizedBox()),
+            const DrawerItem(
+              drawerItemModel: DrawerItemModel(
+                iconPath: AppIcons.iconsSetting,
+                title: 'Setting system',
+              ),
+              isActive: false,
+            ),
+            const DrawerItem(
+              drawerItemModel: DrawerItemModel(
+                iconPath: AppIcons.iconsLogout,
+                title: 'Logout account',
+              ),
+              isActive: false,
+            ),
+          ],
+        ),
       ),
     );
   }
