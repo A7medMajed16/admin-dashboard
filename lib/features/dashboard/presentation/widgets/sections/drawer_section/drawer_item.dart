07256b48efdeb7fd4f8e14cb1de6c429c6a67d3c
A7medMajed16 <ahmedbark129@gmail.com>
2024-03-25 13:18:41 +0200

    "Added `drawerItemModel` as a required parameter to `DrawerItem` constructor"

--- lib/features/dashboard/presentation/widgets/sections/drawer_section/drawer_item.dart
+++ lib/features/dashboard/presentation/widgets/sections/drawer_section/drawer_item.dart
@@ -1,8 +1,10 @@
+import 'package:admin_dashboard/features/dashboard/data/models/drawer_item_model.dart';
 import 'package:flutter/material.dart';
 
 class DrawerItem extends StatelessWidget {
-  const DrawerItem({super.key});
+  const DrawerItem({super.key, required this.drawerItemModel});
 
+  final DrawerItemModel drawerItemModel;
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
