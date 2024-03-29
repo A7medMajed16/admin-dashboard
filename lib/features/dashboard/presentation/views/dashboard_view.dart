9bb4f697da4befce4fc721b61fe135f55ef807c1
A7medMajed16 <ahmedbark129@gmail.com>
2024-03-29 17:19:44 +0200

    "Replace Scaffold with SafeArea and add PreferredSize for AppBar on smaller screens"

--- lib/features/dashboard/presentation/views/dashboard_view.dart
+++ lib/features/dashboard/presentation/views/dashboard_view.dart
@@ -11,53 +11,56 @@ class DashboardView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.sizeOf(context).width;
-    return Scaffold(
-      backgroundColor: const Color(0xffF7F9FA),
-      appBar: width + 32 < 800
-          ? PreferredSize(
-              preferredSize: const Size.fromHeight(kToolbarHeight),
-              child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                child: AppBar(
-                  backgroundColor: const Color(0xff4EB7F2),
-                  centerTitle: true,
-                  iconTheme: const IconThemeData(
-                    color: Colors.white,
-                  ),
-                  actions: [
-                    Padding(
-                      padding: const EdgeInsets.only(right: 12),
-                      child: IconButton(
-                        onPressed: () {},
-                        icon: const Icon(
-                          Icons.logout_rounded,
-                          color: Colors.red,
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: const Color(0xffF7F9FA),
+        appBar: width + 32 < 800
+            ? PreferredSize(
+                preferredSize: const Size.fromHeight(kToolbarHeight),
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                  child: AppBar(
+                    backgroundColor: const Color(0xff4EB7F2),
+                    centerTitle: true,
+                    iconTheme: const IconThemeData(
+                      color: Colors.white,
+                    ),
+                    actions: [
+                      Padding(
+                        padding: const EdgeInsets.only(right: 12),
+                        child: IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.logout_rounded,
+                            color: Colors.red,
+                          ),
                         ),
-                      ),
-                    )
-                  ],
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  title: Text(
-                    'Admin Dashboard',
-                    style: Styles.textStyle24W600.copyWith(color: Colors.white),
+                      )
+                    ],
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    title: Text(
+                      'Admin Dashboard',
+                      style:
+                          Styles.textStyle24W600.copyWith(color: Colors.white),
+                    ),
                   ),
                 ),
-              ),
-            )
-          : null,
-      drawer: Drawer(
-        child: CustomDrawer(
-          userModel: UserModel(
-            imagePath: AppImages.imagesUserFrame,
-            name: 'Ahmed Majed',
-            email: 'demo@gmail.com',
+              )
+            : null,
+        drawer: Drawer(
+          child: CustomDrawer(
+            userModel: UserModel(
+              imagePath: AppImages.imagesUserFrame,
+              name: 'Ahmed Majed',
+              email: 'demo@gmail.com',
+            ),
           ),
         ),
+        body: const DashboardBody(),
       ),
-      body: const DashboardBody(),
     );
   }
 }
